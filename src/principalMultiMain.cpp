2992c6c944b2c5be0c70fe77e6aa28cb296079bd
Franco Quenallata <quenallatafranco@gmail.com>
2025-05-29 17:32:13 -0300

    algoritmosempty

---
+++ src/principalMultiMain.cpp
@@ -0,0 +1,46 @@
+//
+//#ifndef _MAIN
+//#define _MAIN
+//
+//#include <iostream>
+//#include "biblioteca/funciones/strings.hpp"
+//#include "biblioteca/funciones/tokens.hpp"
+//#include "biblioteca/funciones/files.hpp"
+//#include "biblioteca/tads/parte2/Array.hpp"
+//#include "biblioteca/tads/parte2/Map.hpp"
+//#include "biblioteca/tads/parte2/List.hpp"
+//#include "biblioteca/tads/parte2/Stack.hpp"
+//#include "biblioteca/tads/parte2/Queue.hpp"
+//#include "biblioteca/tads/parte1/Coll.hpp"
+//#include "biblioteca/tads/parte1/MultidimColl.hpp"
+//using namespace std;
+//
+//#define EJERCICIO_01
+//
+//#ifdef EJERCICIO_01
+//int main()
+//{
+//   cout << "Ejercicio 1" << endl;
+//   return 0;
+//}
+//#endif
+//
+//
+//#ifdef EJERCICIO_02
+//int main()
+//{
+//   cout << "Ejercicio 2" << endl;
+//   return 0;
+//}
+//#endif
+//
+//#ifdef EJERCICIO_03
+//int main()
+//{
+//   cout << "Ejercicio 3" << endl;
+//   return 0;
+//}
+//#endif
+//
+//
+//#endif
