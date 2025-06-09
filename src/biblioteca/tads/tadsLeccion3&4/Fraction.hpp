475ce2ecd26ed84f9e586cd1ead65d3c80e5c76d
Franco Quenallata <quenallatafranco@gmail.com>
2025-06-08 22:33:47 -0300

    estructuras de tads y planteando funciones

--- src/biblioteca/tads/tadsLeccion3&4/Fraction.hpp
+++ src/biblioteca/tads/tadsLeccion3&4/Fraction.hpp
@@ -5,3 +5,61 @@
 #include <stdlib.h>
 
 using namespace std;
+
+struct Fraction{
+   int numerador;
+   int denominador;
+};
+
+Fraction fraccionNula()
+{
+   Fraction f;
+   f.numerador = 0;
+   f.denominador = 0;
+   return f;
+}
+
+Fraction crearFraccion(int numerador, int denominador)
+{
+
+   if(denominador == 0)
+   {
+      cout << "ERROR: El denominador no puede ser cero" << endl;
+      return fraccionNula();
+   }
+
+   if (denominador < 0) {
+       numerador = -numerador;
+       denominador = -denominador;
+   }
+
+   Fraction f;
+   f.numerador = numerador;
+   f.denominador = denominador;
+   return f;
+}
+
+Fraction sumarFracciones(Fraction a, Fraction b);
+
+Fraction restarFracciones(Fraction a, Fraction b);
+
+Fraction multiplicarFracciones(Fraction a, Fraction b);
+
+Fraction dividirFracciones(Fraction a, Fraction b);
+
+Fraction simplificarFraccion(Fraction f);
+
+Fraction invertirFraccion(Fraction f); // da la inversa
+
+Fraction negativaFraccion(Fraction f); // cambia el signo
+
+int esFraccionValida(Fraction f);
+
+int sonFraccionesIguales(Fraction a, Fraction b);
+
+int esFraccionMayor(Fraction a, Fraction b);
+
+int esFraccionMenor(Fraction a, Fraction b);
+
+void mostrarFraccion(Fraction f);
+
--- src/biblioteca/tads/tadsLeccion3&4/Hour.hpp
+++ src/biblioteca/tads/tadsLeccion3&4/Hour.hpp
@@ -5,3 +5,33 @@
 #include <stdlib.h>
 
 using namespace std;
+
+struct Hour {
+    int hora;
+    int minuto;
+    int segundo;
+};
+
+Hour crearHora(int hora, int minuto);
+
+Hour crearHoraCompleta(int hora, int minuto, int segundo);
+
+Hour horaNula();
+
+string horaAString(Hour h);
+
+void mostrarHora(Hour h);
+
+bool esHoraValida(Hour h);
+
+int aMinutos(Hour h);
+
+int aSegundos(Hour h);
+
+Hour desdeMinutos(int minutos);
+
+Hour normalizarHora(Hour h);
+
+int diferenciaEnMinutos(Hour a, Hour b);
+
+int diferenciaEnSegundos(Hour a, Hour b);
