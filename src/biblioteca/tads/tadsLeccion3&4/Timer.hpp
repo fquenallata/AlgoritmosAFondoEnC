e0cee83718e550d69f9ff9a0f4296dae64ec8732
Franco Quenallata <quenallatafranco@gmail.com>
2025-06-09 12:50:20 -0300

    timers

--- src/biblioteca/tads/tadsLeccion3&4/Timer.hpp
+++ src/biblioteca/tads/tadsLeccion3&4/Timer.hpp
@@ -5,3 +5,31 @@
 #include <stdlib.h>
 
 using namespace std;
+
+struct Timer {
+    int horas;
+    int minutos;
+    int segundos;
+};
+
+Timer crearTimer(int horas, int minutos, int segundos);
+
+Timer desdeSegundos(int totalSegundos);
+
+Timer timerNulo();
+
+Timer iniciarTimer(); // comienza desde cero
+
+Timer reiniciarTimer(Timer t);
+
+Timer pausarTimer(Timer t); // si manejás estados (opcional)
+
+Timer reanudarTimer(Timer t);
+
+Timer sumarTimers(Timer a, Timer b);
+
+Timer restarTimers(Timer a, Timer b);
+
+Timer agregarSegundos(Timer t, int s);
+
+Timer agregarMinutos(Timer t, int m);
